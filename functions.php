b4a63639bd6c16f48f72e1d643da8ba3a6954c77
Aristeides Stathopoulos <aristath@gmail.com>
2016-05-22 16:07:43 +0300

    fhdhfhfdhdf

--- functions.php
+++ functions.php
@@ -4,3 +4,4 @@ add_action( 'wp_enqueue_scripts', function() {
   // Parent theme stylesheet
   wp_enqueue_style( 'twentysixteen-style', trailingslashit( get_template_directory_uri() ) . 'style.css' );
 });
+// gfggf
